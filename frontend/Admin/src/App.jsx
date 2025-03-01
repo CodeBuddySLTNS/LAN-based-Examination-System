09b2f354def1023eee5dafd5cda2a4f7cd2ab3f1
Renz Cole <renz2arjhay@gmail.com>
2025-02-28 19:35:56 -0500

    feat: implement routing for dashboard and accounts pages

--- frontend/Admin/src/App.jsx
+++ frontend/Admin/src/App.jsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
 import { useMainStore } from "./states/store";
 
-import Page from "./app/dashboard/page";
+import Layout from "./app/layout/page";
 import Login from "./app/login/page";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./app/dashboard/page";
+import Accounts from "./app/accounts/page";
 
 function App() {
   const [count, setCount] = useState(0);
   const isLoggedIn = useMainStore((state) => state.isLoggedIn);
   const setIsLoggedIn = useMainStore((state) => state.setIsLoggedIn);
 
-  return <div>{isLoggedIn ? <Page /> : <Login />}</div>;
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Dashboard />
+                </Layout>
+              }
+            />
+            <Route
+              path="/accounts"
+              element={
+                <Layout>
+                  <Accounts />
+                </Layout>
+              }
+            />
+          </Routes>
+        </Router>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 }
 
 export default App;
