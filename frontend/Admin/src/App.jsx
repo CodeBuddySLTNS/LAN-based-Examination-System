ee4f9fd82518454966eb5ecaab6ef6f91c0bf683
Renz Cole <renz2arjhay@gmail.com>
2025-03-02 17:33:55 -0500

    feat: integrate React Query for improved data fetching and state management

--- frontend/Admin/src/App.jsx
+++ frontend/Admin/src/App.jsx
@@ -10,7 +10,6 @@ import Accounts from "./app/accounts/page";
 function App() {
   const [count, setCount] = useState(0);
   const isLoggedIn = useMainStore((state) => state.isLoggedIn);
-  const setIsLoggedIn = useMainStore((state) => state.setIsLoggedIn);
 
   return (
     <div>
--- frontend/Admin/src/main.jsx
+++ frontend/Admin/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./index.css";
 import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
