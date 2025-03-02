b310115bd531ab514b6db3bd25f646f8d6a7cd6c
Renz Cole <renz2arjhay@gmail.com>
2025-03-02 17:33:41 -0500

    feat: add Axios instance for API requests and update login state management

--- frontend/Admin/src/lib/utils.js
+++ frontend/Admin/src/lib/utils.js
@@ -1,6 +1,12 @@
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
+import axios from "axios";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const Axios = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
--- frontend/Admin/src/states/store.js
+++ frontend/Admin/src/states/store.js
@@ -2,5 +2,5 @@ import { create } from "zustand";
 
 export const useMainStore = create((set) => ({
   isLoggedIn: false,
-  setIsLoggedIn: () => set({ isLoggedIn: true }),
+  setIsLoggedIn: (status) => set({ isLoggedIn: status }),
 }));
