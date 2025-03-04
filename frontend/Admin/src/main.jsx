21a120a500d886928da346ed5c0a8acafeca2aaf
Renz Cole <renz2arjhay@gmail.com>
2025-03-04 18:20:21 -0500

    feat: configure React Query client to disable query retries and add user state management in main store

--- frontend/Admin/src/main.jsx
+++ frontend/Admin/src/main.jsx
@@ -5,7 +5,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
