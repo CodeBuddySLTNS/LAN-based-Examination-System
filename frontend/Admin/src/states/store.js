2847331e30ad846aece18e5eb243b631ebddd6b3
Renz Cole <renz2arjhay@gmail.com>
2025-03-03 19:30:59 -0500

    feat: add loading state to main store with setter function

--- frontend/Admin/src/states/store.js
+++ frontend/Admin/src/states/store.js
@@ -1,6 +1,9 @@
 import { create } from "zustand";
 
 export const useMainStore = create((set) => ({
+  isLoading: true,
   isLoggedIn: false,
+
+  setIsLoading: (status) => set({ isLoading: status }),
   setIsLoggedIn: (status) => set({ isLoggedIn: status }),
 }));
