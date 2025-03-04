56685fb4cb3af1332ea5a061aa491a1ac0369cd2
Renz Cole <renz2arjhay@gmail.com>
2025-03-04 18:19:37 -0500

    feat: enhance authentication middleware to handle login and signup without token validation

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -1,28 +1,35 @@
 const jwt = require("jsonwebtoken");
+const { FORBIDDEN } = require("../constants/statusCodes");
 
 const authenticate = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     const secretKey = process.env.SYSTEM_SECRET_KEY;
 
+    // Allow login and signup without token
+    if (req.url === "/auth/login" || req.url === "/auth/signup") {
+      return next();
+    }
+
     if (authHeader && authHeader.startsWith("Bearer")) {
       const token = authHeader.split(" ")[1];
       if (token) {
         jwt.verify(token, secretKey, (err, verifiedToken) => {
           if (err) {
-            return next();
+            return res.status(FORBIDDEN).json({ message: "Invalid token" });
           }
           res.locals.userId = verifiedToken.id;
           next();
+          console.log("logged in");
         });
       } else {
-        next();
+        return res.status(FORBIDDEN).json({ message: "Invalid token" });
       }
     } else {
-      next();
+      return res.status(FORBIDDEN).json({ message: "Invalid token" });
     }
   } catch (e) {
-    next();
+    throw new Error(e);
   }
 };
 
