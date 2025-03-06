67420208f79dcd3f72bdbfc55404cdd594ab621e
Renz Cole <renz2arjhay@gmail.com>
2025-03-05 19:46:18 -0500

    feat: implement user account deletion and refactor user info retrieval in users controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,25 +1,37 @@
 const CustomError = require("../utils/customError");
 const sqlQuery = require("../database/sqlQuery");
+const UserModel = require("../database/models/user");
+
+const User = new UserModel();
 
 const users = async (req, res) => {
   const query = `SELECT * FROM users`;
   const result = await sqlQuery(query);
-  res.json({ users: result });
+  res.json({ users: "result" });
 };
 
 const userInfo = async (req, res) => {
   const userId = res.locals.userId;
-  const query = `SELECT id, name, username, department, year, role
-    FROM users WHERE id = ? LIMIT 1`;
+  const user = User.getUserInfo(userId);
+
+  res.json({ user });
+};
+
+const deleteAccount = async (req, res) => {
+  console.log(req.params);
+  const userId = "";
+  const query = `DELETE FROM users WHERE id = ? LIMIT 1`;
   const result = await sqlQuery(query, [userId]);
+
   if (result.length === 0) {
     throw new CustomError("User not found", 404);
   }
-  console.log(result[0]);
+
   res.json({ user: result[0] });
 };
 
 module.exports = {
   users,
   userInfo,
+  deleteAccount,
 };
