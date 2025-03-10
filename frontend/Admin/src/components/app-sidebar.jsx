7819337aff885b752bb4fceb58f923b8557d6527
Renz Cole <renz2arjhay@gmail.com>
2025-03-09 22:34:22 -0400

    refactor AppSidebar to use state management for teams and navigation items

--- frontend/Admin/src/components/app-sidebar.jsx
+++ frontend/Admin/src/components/app-sidebar.jsx
@@ -1,19 +1,4 @@
 import * as React from "react";
-import {
-  AudioWaveform,
-  School,
-  ClipboardList,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  MonitorCog,
-  ContactRound,
-  FileQuestion,
-  FileClock,
-} from "lucide-react";
 
 import { NavMain } from "@/components/nav-main";
 import { NavProjects } from "@/components/nav-projects";
@@ -26,139 +11,20 @@ import {
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar";
-import { useMainStore } from "@/states/store";
-
-// This is sample data.
-const data = {
-  teams: [
-    {
-      name: "CodeBuddy State College",
-      logo: School,
-      plan: "LAN-Based Examination System",
-    },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
-  navMain: [
-    {
-      title: "Exam Schedules",
-      url: "#",
-      icon: ClipboardList,
-      isActive: false,
-      items: [
-        {
-          title: "March 04, 2025 - Semi-Finals",
-          url: "#",
-        },
-        {
-          title: "February 04, 2025 - Midterm",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Exam History",
-      url: "#",
-      icon: FileClock,
-      isActive: false,
-      items: [
-        {
-          title: "S.Y. 2024 - 2025 2nd Semester",
-          url: "#",
-        },
-        {
-          title: "S.Y. 2024 - 2025 1st Semester",
-          url: "#",
-        },
-        {
-          title: "S.Y. 2023 - 2024 2nd Semester",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Questions Bank",
-      url: "#",
-      icon: FileQuestion,
-      items: [
-        {
-          title: "All Questions",
-          url: "#",
-        },
-        {
-          title: "Add Question",
-          url: "#",
-        },
-        {
-          title: "Edit Question",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Manage Accounts",
-      url: "#",
-      icon: ContactRound,
-      items: [
-        {
-          title: "All Accounts",
-          url: "/accounts",
-        },
-        {
-          title: "Add Account",
-          url: "/accounts/add",
-        },
-      ],
-    },
-    {
-      title: "System",
-      url: "#",
-      icon: MonitorCog,
-      items: [
-        {
-          title: "Logs",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-};
+import { useMainStore, useSidebarStore } from "@/states/store";
 
 export function AppSidebar({ ...props }) {
   const user = useMainStore((state) => state.user);
+  const systemInfo = useSidebarStore((state) => state.systemInfo);
+  const navMain = useSidebarStore((state) => state.navMain);
 
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <TeamSwitcher teams={systemInfo} />
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
+        <NavMain items={navMain} />
         {/* <NavProjects projects={data.projects} /> */}
       </SidebarContent>
       <SidebarFooter>
