b4ad13ba4234c4921f829aabac9534e38cdf4602
Renz Cole <renz2arjhay@gmail.com>
2025-03-11 10:33:06 -0400

    modified:   backend/database/models/question.js

--- backend/database/models/question.js
+++ backend/database/models/question.js
@@ -1,34 +1,69 @@
 const sqlQuery = require("../sqlQuery");
+const { questionBankTableQuery } = require("../tableQueries");
 
 class QuestionModel {
+  async createQuestionsTable() {
+    await sqlQuery(questionBankTableQuery);
+  }
+
   async getAll() {
-    const query = `SELECT * FROM questions`;
+    await this.createQuestionsTable();
+    const query = `SELECT * FROM question_bank`;
     const result = await sqlQuery(query);
     return result;
   }
-  
-  async add(user_id,question, correct_ans, incorrect_ans) {
-    const query = `INSERT INTO questions(
-      user_id, question, correct_answer, incorrect_answer
-      ) VALUES (?, ?, ?, ?)`;
-    const params = [user_id, question, correct_ans, incorrect_ans];
+
+  async addQuestion(qdata) {
+    await this.createQuestionsTable();
+    let query, params;
+
+    if (qdata.questionType === "multiple_choice") {
+      query = `INSERT INTO question_bank(
+        subject, question_text, question_type, choices, correct_answer, created_by
+        ) VALUES (?, ?, ?, ?, ?, ?)`;
+      params = [
+        qdata.subject,
+        qdata.question,
+        qdata.questionType,
+        JSON.stringify(qdata.choices),
+        JSON.stringify(qdata.correctAnswer),
+        qdata.userId,
+      ];
+    } else {
+      query = `INSERT INTO question_bank(
+        subject, question_text, question_type, correct_answer, created_by
+        ) VALUES (?, ?, ?, ?, ?)`;
+      params = [
+        qdata.subject,
+        qdata.question,
+        qdata.questionType,
+        JSON.stringify(qdata.correctAnswer),
+        qdata.userId,
+      ];
+    }
+
     const result = await sqlQuery(query, params);
     return result;
   }
-  
-  async update(question, correct_ans, incorrect_ans) {
-    const query = `INSERT INTO questions(
-      user_id, question, correct_answer, incorrect_answer
-      ) VALUES (?, ?, ?)`;
-    const params = [question, correct_ans, incorrect_ans];
+
+  async editQuestion(qdata) {
+    const query = `INSERT INTO question_bank(
+      subject, question_text, question_type, choices, correct_answer
+      ) VALUES (?, ?, ?, ?, ?)`;
+    const params = [
+      qdata.subject,
+      qdata.question,
+      qdata.questionType,
+      JSON.stringify(qdata.choices),
+      JSON.stringify(qdata.correctAnswer),
+    ];
+
     const result = await sqlQuery(query, params);
     return result;
   }
-  
-  async remove(question, correct_ans, incorrect_ans) {
-    const query = `INSERT INTO questions(
-      user_id, question, correct_answer, incorrect_answer
-      ) VALUES (?, ?, ?)`;
+
+  async deleteQuestion(question_id) {
+    const query = `DELETE FROM question_bank WHERE id = ? LIMIT 1`;
     const params = [question, correct_ans, incorrect_ans];
     const result = await sqlQuery(query, params);
     return result;
