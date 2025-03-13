97be967e9ffcbe0bd3dc7d71eaaf5e36fab2c733
Renz Cole <renz2arjhay@gmail.com>
2025-03-13 14:00:53 -0400

    Refactor tryCatch utility for improved readability

--- backend/utils/tryCatch.js
+++ backend/utils/tryCatch.js
@@ -1,10 +1,7 @@
-const { Request } = require("express");
-
-module.exports.tryCatch =
-  (controller) => async (req, res, next) => {
-    try {
-      await controller(req, res);
-    } catch (error) {
-      return next(error);
-    }
-  };
+module.exports.tryCatch = (controller) => async (req, res, next) => {
+  try {
+    await controller(req, res);
+  } catch (error) {
+    return next(error);
+  }
+};
