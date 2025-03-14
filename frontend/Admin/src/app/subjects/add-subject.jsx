b9e9142e764b22de32d689e3d84015b02096f3c3
Renz Cole <renz2arjhay@gmail.com>
2025-03-13 21:47:45 -0400

    Add new subject management component with form validation and error handling

---
+++ frontend/Admin/src/app/subjects/add-subject.jsx
@@ -0,0 +1,118 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { Axios } from "@/lib/utils";
+import Joi from "joi";
+import { joiResolver } from "@hookform/resolvers/joi";
+import { toast } from "sonner";
+
+const schema = Joi.object({
+  courseCode: Joi.string().label("Course Code").required(),
+  subjectName: Joi.string().label("Subject Name").required(),
+});
+
+export const AddSubject = () => {
+  const postRequest = async (data) => {
+    const token = localStorage.getItem("token");
+    const response = await Axios.post("/subjects/add", data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  };
+
+  const { mutateAsync: addSubject, isPending } = useMutation({
+    mutationFn: postRequest,
+    onSuccess: () => {
+      toast.success("Subject added succesfully.");
+      reset();
+    },
+    onError: (error) => {
+      if (error?.response?.data?.body?.code === "ER_DUP_ENTRY") {
+        return toast.error("This subject already exists.");
+      }
+      if (error?.response?.data?.message) {
+        return toast.error(error.response.data.message);
+      }
+      toast.error("Unable to add this subject.");
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ resolver: joiResolver(schema) });
+
+  const onSubmit = async (data) => {
+    try {
+      addSubject(data);
+    } catch (error) {}
+  };
+
+  return (
+    <div className="p-6 pt-3">
+      <Card>
+        <CardHeader>
+          <CardTitle>Add New Question</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-3">
+                <Label
+                  htmlFor="courseCode"
+                  className="flex items-center gap-3 justify-between"
+                >
+                  Course Code
+                </Label>
+                <Input
+                  {...register("courseCode")}
+                  placeholder="Type the course code here."
+                  id="courseCode"
+                  required
+                />
+
+                {errors?.courseCode?.message && (
+                  <p className="text-sm text-red-600">
+                    {errors.courseCode.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col gap-3">
+                <Label
+                  htmlFor="name"
+                  className="flex items-center gap-3 justify-between"
+                >
+                  Subject Name
+                </Label>
+                <Input
+                  {...register("subjectName")}
+                  placeholder="Type subject name here."
+                  id="name"
+                  required
+                />
+
+                {errors?.name?.message && (
+                  <p className="text-sm text-red-600">{errors.name.message}</p>
+                )}
+              </div>
+            </div>
+            <Button disabled={isPending} type="submit" className="w-full mt-5">
+              Add Question
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AddSubject;
---
+++ frontend/Admin/src/app/subjects/page.jsx
@@ -0,0 +1,579 @@
+"use client";
+
+import * as React from "react";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  ArrowUpDown,
+  Check,
+  ChevronDown,
+  Edit,
+  LucideDelete,
+  MoreHorizontal,
+  XIcon,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Axios } from "@/lib/utils";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useForm } from "react-hook-form";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { toast } from "sonner";
+
+export default function Page() {
+  const [sorting, setSorting] = React.useState([]);
+  const [columnFilters, setColumnFilters] = React.useState([]);
+  const [columnVisibility, setColumnVisibility] = React.useState({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [actionData, setActionData] = React.useState(null);
+  const [isEditDialog, setIsEditDialog] = React.useState(false);
+  const [deleteDialog, setDeleteDialog] = React.useState(false);
+
+  const queryClient = useQueryClient();
+
+  const fetchSubjects = async () => {
+    const token = localStorage.getItem("token");
+    const response = await Axios.get(`/subjects`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  };
+
+  const deleteQuestion = async (data) => {
+    const token = localStorage.getItem("token");
+    const response = await Axios.delete(`/subjects/delete`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data,
+    });
+    return response.data;
+  };
+
+  const editQuestion = async (data) => {
+    const token = localStorage.getItem("token");
+    const response = await Axios.patch(`/subjects/edit`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  };
+
+  const handleAction = async (data) => {
+    let response;
+    switch (data?.action) {
+      case "edit":
+        delete data.action;
+        response = await editQuestion(data);
+        queryClient.invalidateQueries(["subjects"]);
+        return response;
+
+      case "delete":
+        delete data.action;
+        response = await deleteQuestion(data);
+        queryClient.invalidateQueries(["subjects"]);
+        return response;
+
+      default:
+        throw new Error("Invalid action.");
+    }
+  };
+
+  const { data } = useQuery({
+    queryKey: ["subjects"],
+    queryFn: fetchSubjects,
+  });
+
+  const {
+    mutateAsync: action,
+    data: actionResponse,
+    error,
+  } = useMutation({
+    mutationFn: handleAction,
+  });
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const updateBody = {};
+
+    await action({ updateBody, action: "edit" });
+    setIsEditDialog(false);
+  };
+
+  const columns = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Subject
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => <div className="">{row.getValue("name")}</div>,
+    },
+    {
+      accessorKey: "course_code",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Course Code
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="text-center">{row.getValue("course_code")}</div>
+      ),
+    },
+    {
+      accessorKey: "created_by",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Author
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="text-center">{row.getValue("created_by")}</div>
+      ),
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      cell: ({ row }) => {
+        const question = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              {/* <DropdownMenuItem
+                onClick={() => {
+                  setIsEditDialog(true);
+                  setActionData(question);
+                }}
+              >
+                <Edit className="text-green-600" />
+                Edit Question
+              </DropdownMenuItem> */}
+              <DropdownMenuItem
+                onClick={() => {
+                  setDeleteDialog(true);
+                  setActionData(question);
+                }}
+              >
+                <LucideDelete className="text-red-500" />
+                Delete Subject
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+
+  const table = useReactTable({
+    data: data || [],
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  React.useEffect(() => {
+    if (actionResponse) {
+      toast.success(actionResponse.message);
+    }
+
+    if (error) {
+      if (error.response?.data?.message) {
+        toast.error(error.response?.data?.message);
+      } else {
+        toast.error(error.message);
+      }
+    }
+  }, [actionResponse, error]);
+
+  return (
+    <div className=" box-border px-8">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter subject..."
+          value={table.getColumn("name")?.getFilterValue() ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+
+      {/* Dialog for edit account action */}
+      <Dialog open={isEditDialog} onOpenChange={setIsEditDialog}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="lastname" className="text-right">
+                Last Name
+              </Label>
+              <Input
+                {...register("lastname")}
+                id="lastname"
+                type="text"
+                defaultValue={actionData?.name?.split(",")[0]}
+                className="col-span-3"
+                required
+              />
+              {errors?.lastname && (
+                <p className="text-[0.8rem] font-normal text-red-600 flex items-center gap-1">
+                  {errors?.lastname?.message}
+                </p>
+              )}
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="firstname" className="text-right">
+                First Name
+              </Label>
+              <Input
+                {...register("firstname")}
+                id="firstname"
+                type="text"
+                defaultValue={actionData?.name
+                  ?.split(",")[1]
+                  ?.slice(0, -2)
+                  ?.trim()}
+                className="col-span-3"
+                required
+              />
+              {errors?.firstname && (
+                <p className="text-[0.8rem] font-normal text-red-600 flex items-center gap-1">
+                  {errors?.firstname?.message}
+                </p>
+              )}
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="middlename" className="text-right">
+                Middle Name
+              </Label>
+              <Input
+                {...register("middlename")}
+                id="middlename"
+                type="text"
+                defaultValue={actionData?.name?.split(",")[1]?.slice(-2)}
+                className="col-span-3"
+                required
+              />
+              {errors?.middlename && (
+                <p className="text-[0.8rem] font-normal text-red-600 flex items-center gap-1">
+                  {errors?.middlename?.message}
+                </p>
+              )}
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input
+                {...register("username")}
+                id="username"
+                defaultValue={actionData?.username}
+                className="col-span-3"
+                required
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <p className="text-right">Department</p>
+              <div className="col-span-3">
+                <Select
+                  defaultValue={actionData?.department}
+                  onValueChange={(value) => setValue("department", value)}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select department" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="BSCS">BSCS</SelectItem>
+                      <SelectItem value="BSSW">BSSW</SelectItem>
+                      <SelectItem value="BSIT">BSIT</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="year" className="text-right">
+                Year
+              </Label>
+              <Input
+                {...register("year")}
+                id="year"
+                type="number"
+                className="col-span-3"
+                min="1"
+                max="4"
+                defaultValue={actionData?.year}
+                required
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+
+      {/* Alert Dialog for delete account action */}
+      <AlertDialog open={deleteDialog} onOpenChange={setDeleteDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete this
+              question and remove the data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                try {
+                  action({ questionId: actionData?.id, action: "delete" });
+                } catch (error) {}
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+}
