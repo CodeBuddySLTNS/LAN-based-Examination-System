6ccb81f956ee4ef32b1de4eaf84c4a387a67beba
Renz Cole <renz2arjhay@gmail.com>
2025-03-17 09:35:43 +0800

    add routes for exam creation, editing, and deletion

--- backend/routes/exams.js
+++ backend/routes/exams.js
@@ -4,5 +4,8 @@ const controller = require("../controllers/exams");
 const Router = express.Router();
 
 Router.get("/", tryCatch(controller.exams));
+Router.post("/add", tryCatch(controller.addExam));
+Router.patch("/edit", tryCatch(controller.editExam));
+Router.delete("/delete", tryCatch(controller.deleteExam));
 
 module.exports = Router;
