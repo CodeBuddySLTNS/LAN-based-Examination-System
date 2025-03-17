6f0d7b32da54e0a2239b770f5dd79a88eeca0ad9
Renz Cole <renz2arjhay@gmail.com>
2025-03-17 20:28:35 +0800

    feat: add Axios2 utility function for flexible API requests with token authorization

--- frontend/Admin/src/lib/utils.js
+++ frontend/Admin/src/lib/utils.js
@@ -10,3 +10,44 @@ export const Axios = axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
+
+export const Axios2 = (endpoint, method) => async (data) => {
+  const token = localStorage.getItem("token");
+  let response;
+
+  switch (method?.toUpperCase()) {
+    case "POST":
+      response = await Axios.post(endpoint, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+
+    case "PATCH":
+      response = await Axios.patch(endpoint, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+
+    case "DELETE":
+      response = await Axios.delete(endpoint, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: data,
+      });
+      return response.data;
+
+    default:
+      // GET request
+      response = await Axios.get(endpoint, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+  }
+};
