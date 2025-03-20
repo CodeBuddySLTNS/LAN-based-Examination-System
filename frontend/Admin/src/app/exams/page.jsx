53e64a1f064e4366461866ec6b49204ad3f51369
Renz Cole <renz2arjhay@gmail.com>
2025-03-20 10:37:43 +0800

    feat: implement dialog for viewing and editing exam questions in DataTable

--- frontend/Admin/src/app/exams/page.jsx
+++ frontend/Admin/src/app/exams/page.jsx
@@ -1,17 +1,55 @@
 import DataTable from "@/components/data-table";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Axios, Axios2 } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
-import { ArrowUpDown, LucideDelete, MoreHorizontal } from "lucide-react";
+import { ArrowUpDown } from "lucide-react";
+import { useState } from "react";
 
 const Page = () => {
-  const { data } = useQuery({
+  const [questionsDialog, setQuestionsDialog] = useState({
+    open: false,
+    data: null,
+    initialSelectedRows: {},
+  });
+  const [selectedQuestions, setSelectedQuestions] = useState([]);
+
+  const { data: exams } = useQuery({
     queryKey: ["exams"],
     queryFn: Axios2("/exams", "GET"),
   });
 
-  const columns = [
+  const { data: questions } = useQuery({
+    queryKey: ["questions"],
+    queryFn: Axios2("/questions", "GET"),
+  });
+
+  const handleOpenQuestionsDialog = ({ row, table }) => {
+    const questions = row.original.questions
+      ? JSON.parse(row.original.questions)
+      : [];
+    setQuestionsDialog({
+      open: true,
+      data: { questions },
+      initialSelectedRows: questions,
+    });
+    setSelectedQuestions(questions);
+  };
+
+  const examColumns = [
     {
       id: "select",
       header: ({ table }) => (
@@ -101,8 +139,13 @@ const Page = () => {
           </Button>
         );
       },
-      cell: ({ row }) => (
-        <div className="text-center hover:underline">View/Edit</div>
+      cell: ({ row, table }) => (
+        <div
+          className="text-center hover:underline"
+          onClick={() => handleOpenQuestionsDialog({ row, table })}
+        >
+          View/Edit
+        </div>
       ),
     },
     {
@@ -151,13 +194,218 @@ const Page = () => {
     },
   ];
 
+  const questionColumns = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => {
+            table.toggleAllPageRowsSelected(!!value);
+            value
+              ? setSelectedQuestions(
+                  table.getFilteredRowModel().rows.map((r) => r.original)
+                )
+              : setSelectedQuestions([]);
+          }}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row, table }) => {
+        const isSelected = selectedQuestions.some(
+          (q) => q.question_text === row.original.question_text
+        );
+
+        return (
+          <Checkbox
+            checked={isSelected || row.getIsSelected()}
+            onCheckedChange={(value) => {
+              row.toggleSelected(!!value);
+              value
+                ? setSelectedQuestions((prev) => [...prev, row.original])
+                : setSelectedQuestions((prev) =>
+                    prev.filter((q) => q.id !== row.original.id)
+                  );
+            }}
+            aria-label="Select row"
+          />
+        );
+      },
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "subject",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            type="button"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Subject
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="text-center">{row.getValue("subject")}</div>
+      ),
+    },
+    {
+      accessorKey: "question_text",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            type="button"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Question
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="">{row.getValue("question_text")}</div>
+      ),
+    },
+    {
+      accessorKey: "question_type",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            type="button"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Question Type
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="text-center">{row.getValue("question_type")}</div>
+      ),
+    },
+    {
+      accessorKey: "choices",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            type="button"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Choices
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) =>
+        JSON.parse(row.getValue("choices"))?.length > 0 ? (
+          <div className="text-center">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger type="button">View</TooltipTrigger>
+                <TooltipContent>
+                  {JSON.parse(row.getValue("choices"))?.join(", ")}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+        ) : (
+          <div className="text-center">N/A</div>
+        ),
+    },
+    {
+      accessorKey: "correct_answer",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            type="button"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Correct Answer
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="text-center">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger type="button">View</TooltipTrigger>
+              <TooltipContent>
+                {JSON.parse(row.getValue("correct_answer"))?.join(", ")}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      ),
+    },
+    {
+      accessorKey: "created_by",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="w-full"
+            type="button"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Author
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="text-center">{row.getValue("created_by")}</div>
+      ),
+    },
+  ];
+
   return (
     <div className="p-6 pt-3">
       <DataTable
-        data={data}
-        columns={columns}
+        data={exams}
+        columns={examColumns}
         filter={{ column: "subject", placeholder: "subject (course code)" }}
       />
+
+      <Dialog
+        open={questionsDialog.open}
+        onOpenChange={(open) =>
+          setQuestionsDialog({ ...questionsDialog, open })
+        }
+      >
+        <DialogContent className="md:w-full md:max-w-[90dvw]">
+          <DialogHeader>
+            <DialogTitle>Exam Questions</DialogTitle>
+            <DialogDescription>Add or remove questions here.</DialogDescription>
+          </DialogHeader>
+          <div className="w-full">
+            <DataTable
+              data={questions}
+              columns={questionColumns}
+              filter={{
+                column: "subject",
+                placeholder: "subject (course code)",
+              }}
+              initialSelectedRows={questionsDialog.initialSelectedRows}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
