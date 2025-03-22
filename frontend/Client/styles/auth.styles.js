fdfb69a0d9fb82367aa9157c7835d8ce7a275ad8
Renz Cole <renz2arjhay@gmail.com>
2025-03-22 21:32:34 +0800

    feat: add styles for authentication and home screens

---
+++ frontend/Client/styles/auth.styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loginForm: {
+    width: "85%",
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    gap: 10,
+  },
+  formTitle: {
+    textAlign: "center",
+    fontSize: 19,
+    fontWeight: "bold",
+  },
+  input: {
+    width: "100%",
+    borderColor: "#999",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  inputLabel: {
+    paddingVertical: 1.5,
+  },
+  errorText: {
+    color: "red",
+  },
+});
