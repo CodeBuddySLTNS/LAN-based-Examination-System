765cb9fc0ef35a8dd54ebc6656e9f66d2fde7e00
Renz Cole <renz2arjhay@gmail.com>
2025-03-22 21:33:04 +0800

    feat: enhance layout with SafeAreaProvider and add home screen component

--- frontend/Client/app/_layout.tsx
+++ frontend/Client/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack screenOptions={{ animation: "slide_from_bottom" }}>
+          <Stack.Screen
+            name="index"
+            options={{ title: "Home", animation: "fade_from_bottom" }}
+          />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
--- frontend/Client/app/index.jsx
+++ frontend/Client/app/index.jsx
@@ -1,42 +1,20 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import PokemonCard from "./components/PokemonCard";
+import { styles } from "@/styles/home.styles";
+import { Link, Redirect } from "expo-router";
+import { useMainStore } from "@/states/store";
 
 const index = () => {
-  const pokemons = [
-    {
-      name: "Charmander",
-      image: require("@/assets/images/charmander.png"),
-      type: "Fire",
-      hp: 39,
-      moves: ["Scratch", "Ember", "Growl", "Leer"],
-      weaknesses: ["Water", "Rock"],
-    },
-    {
-      name: "Bulbasaur",
-      image: require("@/assets/images/bulbasaur.png"),
-      type: "Grass",
-      hp: 45,
-      moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
-      weaknesses: ["Fire", "Ice", "Flying", "Physic"],
-    },
-  ];
+  const isLoggedIn = useMainStore((state) => state.isLoggedIn);
+
+  if (!isLoggedIn) return <Redirect href="/login" />;
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.contents}>
-        <PokemonCard {...pokemons[0]} />
-        <PokemonCard {...pokemons[1]} />
-        <PokemonCard {...pokemons[1]} />
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome Home Bebeluvs.</Text>
+      <Link href="/login">Login</Link>
+    </View>
   );
 };
 
 export default index;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  contents: {},
-});
