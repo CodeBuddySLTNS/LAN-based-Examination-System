81bab23a01b825f421819c7e8caebc98c01c13d9
Renz Cole <renz2arjhay@gmail.com>
2025-03-24 19:46:13 +0800

    modified:   frontend/Client/app/index.jsx

--- frontend/Client/app/index.jsx
+++ frontend/Client/app/index.jsx
@@ -1,14 +1,35 @@
 import { Text, View } from "react-native";
-import React from "react";
-import { styles } from "@/styles/home.styles";
-import { Link } from "expo-router";
+import React, { useEffect } from "react";
 import { QueryProvider } from "@/wrapper/query-provider";
+import Homepage from "./(drawer)/home";
+import { useQuery } from "@tanstack/react-query";
+import { Axios2 } from "@/lib/utils";
+import { useRootNavigationState, useRouter } from "expo-router";
+import { useMainStore } from "@/states/store";
 
 const IndexPage = () => {
+  const router = useRouter();
+  const navState = useRootNavigationState();
+  const { data: user, error } = useQuery({
+    queryKey: ["user"],
+    queryFn: Axios2("/users/user/me"),
+  });
+
+  useEffect(() => {
+    if (user) {
+      useMainStore.getState().setUser(user.user);
+      if (!navState?.key) return;
+      router.replace("/home");
+    }
+    if (error) {
+      if (!navState?.key) return;
+      router.replace("/login");
+    }
+  }, [user, error, navState?.key]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome Home Bebeluvs.</Text>
-      <Link href="/login">Login</Link>
+    <View style={{ flex: 1 }}>
+      <Text>Loading...</Text>
     </View>
   );
 };
@@ -16,7 +37,9 @@ const IndexPage = () => {
 const index = () => {
   return (
     <QueryProvider>
-      <IndexPage />
+      <View style={{ flex: 1 }}>
+        <IndexPage />
+      </View>
     </QueryProvider>
   );
 };
