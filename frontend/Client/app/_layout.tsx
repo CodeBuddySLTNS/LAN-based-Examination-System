a749a365681252008b6bf2752227fd827106c6a8
Renz Cole <renz2arjhay@gmail.com>
2025-03-24 19:46:40 +0800

    modified:   frontend/Client/app/_layout.tsx

--- frontend/Client/app/_layout.tsx
+++ frontend/Client/app/_layout.tsx
@@ -1,41 +1,15 @@
-import { useMainStore } from "@/states/store";
-import { Redirect, Stack } from "expo-router";
-import { useEffect } from "react";
-import { Text } from "react-native";
+import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  const isLoggedIn = useMainStore((state) => state.isLoggedIn);
-  const isLoading = useMainStore((state) => state.isLoading);
-
-  useEffect(() => {
-    useMainStore.getState().setIsLoading(false);
-  }, []);
-
-  if (isLoading) {
-    return (
-      <SafeAreaProvider>
-        <SafeAreaView
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <Text>Loading...</Text>
-        </SafeAreaView>
-      </SafeAreaProvider>
-    );
-  }
-
-  if (!isLoggedIn) return <Redirect href="/login" />;
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <Stack screenOptions={{ animation: "slide_from_bottom" }}>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
           <Stack.Screen
-            name="index"
-            options={{ title: "Home", animation: "fade_from_bottom" }}
-          />
-          <Stack.Screen
-            name="login"
+            name="(drawer)"
             options={{ animation: "slide_from_bottom", headerShown: false }}
           />
         </Stack>
