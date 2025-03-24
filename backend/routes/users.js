45c8c532cf8e5cd72ffae5b900d8899c3a2bc19a
Renz Cole <renz2arjhay@gmail.com>
2025-03-24 19:45:16 +0800

    modified:   backend/routes/users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,7 @@ const controller = require("../controllers/users");
 const router = Router();
 
 router.get("/", tryCatch(controller.users));
-router.get("/user/:username", tryCatch(controller.userInfo));
+router.get("/user/me", tryCatch(controller.userInfo));
 router.patch("/user/edit", tryCatch(controller.editAccount));
 router.delete("/user/delete", tryCatch(controller.deleteAccount));
 router.patch("/user/verify", tryCatch(controller.verifyAccount));
