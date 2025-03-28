5fe3a70a23e8ce79b604e76aa3eebfa01e63e526
Renz Cole <renz2arjhay@gmail.com>
2025-03-28 17:42:20 +0800

    feat: update theme colors in themes.js for improved UI aesthetics

--- frontend/Client/constants/themes.js
+++ frontend/Client/constants/themes.js
@@ -1,9 +1,8 @@
 export const light = {
   bg: "#0e0a3f",
   bgVariant: "#0c004b",
-  primary: "#00b3ff",
-  primaryVariant: "#0d5399",
-  white: "#fafafa",
-  darkWhite: "#f0f0f0",
-  black: "#000",
+  primary: "#03346E",
+  secondary: "#6EACDA",
+  background: "#E2E2B6",
+  color: "#fafafa",
 };
