c639adeed882dad687c18ea6a013b667f0a4b00d
Renz Cole <renz2arjhay@gmail.com>
2025-03-29 21:37:06 +0800

    feat: implement socket connection management in store

--- frontend/Client/states/store.js
+++ frontend/Client/states/store.js
@@ -1,11 +1,43 @@
 import { create } from "zustand";
+import { io } from "socket.io-client";
+import config from "@/system.config.json";
 
 export const useMainStore = create((set) => ({
   user: null,
-  isLoading: true,
   isLoggedIn: true,
+  isOnline: false,
+  socket: null,
 
-  setIsLoading: (status) => set({ isLoading: status }),
   setIsLoggedIn: (status) => set({ isLoggedIn: status }),
+  setIsOnline: (status) => set({ isOnline: status }),
   setUser: (user) => set({ user }),
+  setSocket: (socket) => set({ socket }),
+}));
+
+export const useSocketStore = create((set, get) => ({
+  socket: null,
+
+  initializeSocket: () => {
+    if (!get().socket) {
+      const url = config.isProduction
+        ? config.productionServer
+        : config.developmentServer;
+
+      const socket = io(url);
+
+      socket.on("connect", () => {
+        console.log("you are now connected");
+      });
+
+      socket.on("disconnect", (connection) => {
+        console.log("you were disconnected", connection);
+      });
+
+      socket.on("connect_error", (connection) => {
+        console.log("connect error:", connection.message);
+      });
+
+      set({ socket });
+    }
+  },
 }));
