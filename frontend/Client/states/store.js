26ad8f1164e318db4d6e242da66443a4d6e7007b
Renz Cole <renz2arjhay@gmail.com>
2025-03-30 09:54:56 +0800

    feat: enhance socket initialization to show toast notifications on connect and disconnect

--- frontend/Client/states/store.js
+++ frontend/Client/states/store.js
@@ -1,23 +1,22 @@
 import { create } from "zustand";
 import { io } from "socket.io-client";
 import config from "@/system.config.json";
+import ShowToast from "@/components/show-toast";
 
 export const useMainStore = create((set) => ({
   user: null,
   isLoggedIn: true,
   isOnline: false,
-  socket: null,
 
   setIsLoggedIn: (status) => set({ isLoggedIn: status }),
   setIsOnline: (status) => set({ isOnline: status }),
   setUser: (user) => set({ user }),
-  setSocket: (socket) => set({ socket }),
 }));
 
 export const useSocketStore = create((set, get) => ({
   socket: null,
 
-  initializeSocket: (query) => {
+  initializeSocket: (query, toast, router) => {
     if (!get().socket) {
       const url = config.isProduction
         ? config.productionServer
@@ -28,11 +27,22 @@ export const useSocketStore = create((set, get) => ({
       socket.on("connect", () => {
         console.log("you are now connected");
         useMainStore.getState().setIsOnline(true);
+        toast.show({
+          id: 0,
+          render: () => <ShowToast message="You are now online." />,
+        });
       });
 
       socket.on("disconnect", (connection) => {
         console.log("you were disconnected", connection);
         useMainStore.getState().setIsOnline(false);
+        toast.show({
+          id: 0,
+          duration: null,
+          render: () => (
+            <ShowToast action="error" message="You are currently offline." />
+          ),
+        });
       });
 
       set({ socket });
